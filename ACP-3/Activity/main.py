1ef29a2dda6fcee2f68238e9bf5d86157a158cc7
Anshubro-cyber <akshatagrawal0831@gmail.com>
2024-08-22 17:02:51 +0530

    ACP-3

---
+++ ACP-3/Activity/main.py
@@ -0,0 +1,10 @@
+print("Welcome to the exam eligiblity test")
+print("------------------------------------")
+age = int(input("Enter your age:"))
+if age>=18:
+    print("You are eligible for the test")
+else: 
+    print("You are not eligible for the test")
+
+   
+
