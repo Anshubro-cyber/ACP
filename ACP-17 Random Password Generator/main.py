ab3c077161deb0d90b4d78ca85ba7f4187ef425e
Anshubro-cyber <akshatagrawal0831@gmail.com>
2024-10-11 12:15:06 +0530

    ACP-17 Random Password Generator

---
+++ ACP-17 Random Password Generator/main.py
@@ -0,0 +1,19 @@
+import random
+import tkinter as tk
+ 
+
+def generate_password():
+    password_length = 8  # Length of the password
+    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+"
+    password = ''.join(random.choice(characters) for _ in range(password_length))
+    password_label.config(text=password)
+
+window = tk.Tk()
+window.geometry("700x500")
+generate_button = tk.Button(window, text="Generate Password", command=generate_password)
+generate_button.pack()
+
+password_label = tk.Label(window, text="")
+password_label.pack()
+
+window.mainloop()
