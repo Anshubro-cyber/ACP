3d9608a62e30a0dafcc42b5f0dffd38e567be3c6
Anshubro-cyber <akshatagrawal0831@gmail.com>
2024-11-19 16:59:06 +0530

    ACP-27 FirstSetBit

--- ACP-27 FirstSetBit/ACP-27 FirstSetBit/main.py
+++ ACP-27 FirstSetBit/ACP-27 FirstSetBit/main.py
@@ -1,15 +1,13 @@
 def find_rightmost_set_bit(n):
-    # The rightmost set bit is found using n & -n
-    # This isolates the rightmost set bit
+    
     if n == 0:
-        return -1  # No set bits in 0
-    position = (n & -n).bit_length()  # bit_length() gives the position of the rightmost set bit
+        return -1  
+    position = (n & -n).bit_length()  
     return position
 
-# Input from the user
 number = int(input("Enter a number: "))
 
-# Find the position of the rightmost set bit
+
 position = find_rightmost_set_bit(number)
 
 if position == -1:
